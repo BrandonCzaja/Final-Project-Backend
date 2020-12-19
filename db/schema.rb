8133f1ce9773986241ea30f3e83139001f6caf16
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-19 14:14:34 -0600

    Seed User DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_040616) do
+ActiveRecord::Schema.define(version: 2020_12_19_200814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2020_12_16_040616) do
     t.string "image"
     t.string "genus"
     t.string "family"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,34 +17,35 @@
 
 
 # PLANTS BY NORTH AMERICAN REGION
-response = HTTParty.get("https://trefle.io/api/v1/distributions/7/plants?token=tM_vyRwHmo__kNvStVE0N3950_E7eGC8nyoCqmZhEuA")
-    result = JSON.parse(response.body)
+# response = HTTParty.get("https://trefle.io/api/v1/distributions/7/plants?token=tM_vyRwHmo__kNvStVE0N3950_E7eGC8nyoCqmZhEuA")
+#     result = JSON.parse(response.body)
 
-    plant_data = result['data'].map do |element|
-      data = { 
-        api_id: element['id'],
-        common_name: element['common_name'],
-        slug: element['slug'],
-        scientific_name: element['scientific_name'],
-        year: element['year'],
-        rank: element['rank'],
-        family_common_name: element['family_common_name'],
-        image: element['image_url'],
-        genus: element['genus'],
-        family: element['family']
-       }
+#     plant_data = result['data'].map do |element|
+#       data = { 
+#         api_id: element['id'],
+#         common_name: element['common_name'],
+#         slug: element['slug'],
+#         scientific_name: element['scientific_name'],
+#         year: element['year'],
+#         rank: element['rank'],
+#         family_common_name: element['family_common_name'],
+#         image: element['image_url'],
+#         genus: element['genus'],
+#         family: element['family']
+#        }
 
-       new_plant = Plant.new(data)
-       new_plant.save()
-    end
-
-
-
-p "Created #{Plant.count} plants"
-puts "Seed Complete"
+#        new_plant = Plant.new(data)
+#        new_plant.save()
+#     end
 
 
 
+# p "Created #{Plant.count} plants"
+# puts "Seed Complete"
+
+
+user = User.create(username: 'Test', password: 'Test')
+user = User.create(username: 'Test2', password: 'Test2')
 
 
 
